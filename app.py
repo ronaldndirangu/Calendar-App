154be786c56ca90d0da922b22eed9902878faf6d
didasmbalanya <didasmbalanya@gmail.com>
2016-10-14 12:15:12 +0300

    fixes bugs

--- app.py
+++ app.py
@@ -12,37 +12,44 @@ class AppRun(object):
         print("\t+++++++++++++++++++++++++++++++++")
 
     def user_command(self):
-        print("Press[1] to view a list of events")
-        print("Press[2] to add event to Calender")
-        print("Press[3] to view the last event")
-        print("press 'q' to quit!")
 
-        selection  = input("Enter 1,2,3 or 4 \n")
+        print("Press[1] to create callendar")
+        print("Press[2] to view a list of events")
+        print("Press[3] to add event to Calender")
+        print("Press[4] to view the last event")
+        print("\npress 'q' to quit!")
+
+        selection  = input("\nEnter 1,2,3 or 4 \n")
         return selection
-
 
 #Main app part
 app = AppRun()
 app.display_title()
-selection = ""
+selection = app.user_command()
+
 while selection != 'q':
 
     app.display_title()
     if selection == "1":
-        print("Current Events are:\n")
+        print("Enter a name of the type of calendar you want:\n")
         #display_events()
         input("Press Enter to continue")
 
     elif selection == "2":
+        print("A view of a list of events")
         #add_event()
-        print("Event has Been added")
         input("Press Enter to continue")
 
     elif selection == "3":
+        #app.display_event()
+        print("Event Added to calendar\n")
+        input("Press Enter to continue")
+
+    elif selection == "4":
         print("Last Event is \n")
-        #last_event()
+        #app.display_last_event()
         input("Press Enter to continue")
 
     else:
-        print("Invalid choice try again")
+        print("Invalid choice try again\n")
     selection = app.user_command()
